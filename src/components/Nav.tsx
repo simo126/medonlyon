a976dbfbfb929040c1f51224cfe7ebdadc35aa4f
Mohammed Sbaiti <simo82220@gmail.com>
2024-07-22 23:06:03 +0100

    New component

---
+++ src/components/Nav.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Nav = () => {
+  return (
+    <nav className="text-center w-full bg-black">
+      <h1 className="text-center w-full">medonlyon</h1>
+    </nav>
+  );
+};
+
+export default Nav;
